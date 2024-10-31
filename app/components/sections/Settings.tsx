2aaa9547d556bfc11241da1703cd1169de8523f8
MinThant <minthant180@gmail.com>
2024-10-31 15:29:13 +0900

    feat:layout structure

---
+++ app/components/sections/Settings.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Settings = () => {
+    return (
+        <div>
+            <h2>Setting Overview</h2>
+            <p>This is the detailed analytics page with specific content for setting.</p>
+        </div>
+    );
+};
+
+export default Settings;
