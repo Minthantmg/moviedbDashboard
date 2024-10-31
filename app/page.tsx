b89cd973ac2bb4c6846dd70a4a386e014212c766
MinThant <minthant180@gmail.com>
2024-10-31 15:29:08 +0900

    feat:layout structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Link from "next/link";
 
 const Page = () => {
     return (
-        <div>
-          hello
-        </div>
+        <main className="flex items-center justify-center h-screen">
+            <div className="text-center">
+                <h1>Welcome to MovieDB Dashboard</h1>
+                <p>
+                    Go to your <Link href="/dashboard">Dashboard</Link>
+                </p>
+            </div>
+        </main>
     );
 };
 
