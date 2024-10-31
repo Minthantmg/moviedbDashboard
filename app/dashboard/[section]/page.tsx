c5719c8adbcb3800968e2bf49f32957e3509a17b
MinThant <minthant180@gmail.com>
2024-10-31 17:24:00 +0900

    fix:update sidebar handle component

--- app/dashboard/[section]/page.tsx
+++ app/dashboard/[section]/page.tsx
@@ -1,37 +1,51 @@
 'use client'
-import Settings from '../../components/sections/Settings';
 import { useSidebar } from "@/app/components/contexts/SidebarContext";
 import { useEffect, useState } from "react";
+import Settings from '../../components/sections/Settings';
+import Users from '@/app/components/sections/Users';
+import Dashboard from '@/app/components/sections/Dashboard';
+import { usePathname } from "next/navigation";
+
+const sectionComponents: { [key: string]: React.ComponentType } = {
+    settings: Settings,
+    users: Users,
+    dashboard: Dashboard
+};
 
 export default function SectionPage({ params }: { params: { section: string } | Promise<{ section: string }> }) {
     const [section, setSection] = useState<string | null>(null);
     const { setShowSidebar } = useSidebar();
+    const pathname = usePathname();
 
     useEffect(() => {
         const loadSection = async () => {
             const resolvedParams = await (params instanceof Promise ? params : Promise.resolve(params));
             setSection(resolvedParams.section);
         };
-        loadSection()
+        loadSection();
     }, [params]);
 
     useEffect(() => {
-        setShowSidebar(section === 'settings');
-        return () => setShowSidebar(true);
-    }, [section, setShowSidebar]);
+        // Show the sidebar for all /dashboard/* route
+        const isDashboardRoute = pathname.startsWith('/');
+        setShowSidebar(isDashboardRoute);
+
+        return () => setShowSidebar(false); // Hide sidebar on route changes away
+    }, [pathname, setShowSidebar]);
 
     if (section === null) {
         return <div>Loading...</div>;
     }
 
-    switch (section) {
-        case 'settings':
-            return <Settings />;
-        default:
-            return (
-                <div className="flex justify-center items-center h-screen">
-                    {`${section} route does not exist.`}
-                </div>
-            );
-    }
+    // Get the component for the section if it exists
+    const SectionComponent = sectionComponents[section];
+
+    // If no component is found for the section, show a 404 message
+    return SectionComponent ? (
+        <SectionComponent />
+    ) : (
+        <div className="flex justify-center items-center h-screen">
+            {`${section} route does not exist.`}
+        </div>
+    );
 }
