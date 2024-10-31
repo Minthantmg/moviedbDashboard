f1ac09faed8746e9247361e9de8c1c0869cfc922
MinThant <minthant180@gmail.com>
2024-10-31 17:23:37 +0900

    feat:create sidebar ui

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,5 +1,31 @@
+import {sidebarItems} from "@/constants/sideBarItems";
+import Link from "next/link";
+import {Menu} from 'lucide-react'
+import movie from '../public/movie.png'
+import Image from "next/image";
+
 const Sidebar = () => {
-    return <div>Sidebar Content</div>;
+    return (
+        <div className="min-h-screen border-r-2">
+            <div className='flex justify-between items-center px-2 pt-4 pb-16'>
+                <div className="flex justify-center items-center gap-2">
+                    <Image src={movie} alt="title_img" width={32} height={32}/>
+                    MovieDb
+                </div>
+                <Menu />
+            </div>
+            {sidebarItems.map((item) => (
+                <Link
+                    href={item.url}
+                    key={item.title}
+                    className="py-4 px-4 flex items-center mx-2 sidebarItem"
+                >
+                    <div className="mr-2">{item.icon}</div>
+                    <h1 className="ml-2">{item.title}</h1>
+                </Link>
+            ))}
+        </div>
+    )
 };
 
 export default Sidebar;
