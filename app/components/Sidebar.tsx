9710c2a0a878ee743caf14483c39791179d73e9c
MinThant <minthant180@gmail.com>
2024-11-01 15:29:41 +0900

    fix:update ui

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -6,24 +6,29 @@ import Image from "next/image";
 
 const Sidebar = () => {
     return (
-        <div className="min-h-screen border-r-2">
-            <div className='flex justify-between items-center px-2 pt-4 pb-16'>
-                <div className="flex justify-center items-center gap-2">
-                    <Image src={movie} alt="title_img" width={32} height={32}/>
-                    MovieDb
+        <div className="min-h-screen flex flex-col justify-between border-r-2">
+            <div>
+                <div className='flex justify-between items-center px-2 pt-4 pb-16'>
+                    <div className="flex justify-center items-center gap-2">
+                        <Image src={movie} alt="title_img" width={32} height={32}/>
+                        MovieDb
+                    </div>
+                    <Menu/>
                 </div>
-                <Menu />
+                {sidebarItems.map((item) => (
+                    <Link
+                        href={item.url}
+                        key={item.title}
+                        className="py-4 px-4 flex items-center mx-2 sidebarItem"
+                    >
+                        <div className="mr-2">{item.icon}</div>
+                        <h1 className="ml-3">{item.title}</h1>
+                    </Link>
+                ))}
             </div>
-            {sidebarItems.map((item) => (
-                <Link
-                    href={item.url}
-                    key={item.title}
-                    className="py-4 px-4 flex items-center mx-2 sidebarItem"
-                >
-                    <div className="mr-2">{item.icon}</div>
-                    <h1 className="ml-2">{item.title}</h1>
-                </Link>
-            ))}
+            <div className='flex items-center justify-center'>
+                v1.0.0
+            </div>
         </div>
     )
 };
