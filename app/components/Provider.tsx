3fb1963a65315ee1e55d98839f4ff733eb9bfa84
MinThant <minthant180@gmail.com>
2024-11-01 15:32:26 +0900

    fix:type error

--- app/components/Provider.tsx
+++ app/components/Provider.tsx
@@ -2,7 +2,7 @@
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {PropsWithChildren} from "react";
 
-const Providers = ({children}: PropsWithChildren<{}>) => {
+const Providers = ({children}: PropsWithChildren<unknown>) => {
 
     const queryClient = new QueryClient()
     return <QueryClientProvider client={queryClient}>
