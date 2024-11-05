54ad858422f1ee9d3b976faee3785d0b89868391
MinThant <minthant180@gmail.com>
2024-11-05 17:18:16 +0900

    feat:detail view and delete dialog

--- app/components/sections/movies/columns.tsx
+++ app/components/sections/movies/columns.tsx
@@ -12,6 +12,23 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
 
 export const columns: ColumnDef<Movie>[] = [
     {
@@ -77,6 +94,7 @@ export const columns: ColumnDef<Movie>[] = [
         id: "actions",
         cell: ({ row }) => {
             const movie = row.original
+            // eslint-disable-next-line react-hooks/rules-of-hooks
             console.log(movie)
             return (
                 <DropdownMenu>
@@ -87,9 +105,42 @@ export const columns: ColumnDef<Movie>[] = [
                         </Button>
                     </DropdownMenuTrigger>
                     <DropdownMenuContent align="end">
-                        <DropdownMenuItem>View Movie</DropdownMenuItem>
-                        <DropdownMenuItem>Delete</DropdownMenuItem>
+                        <DropdownMenuItem asChild>
+                            <Sheet>
+                                <SheetTrigger asChild>
+                                    <div className="text-sm cursor-pointer py-2
+                                    hover:bg-gray-100 px-2">View Detail</div>
+                                </SheetTrigger>
+                                <SheetContent>
+                                    <SheetHeader>
+                                        <SheetTitle>{movie.title}</SheetTitle>
+                                        <SheetDescription>
+                                            {movie.description}
+                                        </SheetDescription>
+                                    </SheetHeader>
+                                </SheetContent>
+                            </Sheet>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem asChild>
+                            <Dialog>
+                                <DialogTrigger asChild>
+                                    <div className="text-sm cursor-pointer py-2
+                                    hover:bg-gray-100 px-2">Delete
+                                    </div>
+                                </DialogTrigger>
+                                <DialogContent>
+                                    <DialogHeader>
+                                    <DialogTitle>Are you absolutely sure?</DialogTitle>
+                                        <DialogDescription>
+                                            This action cannot be undone. This will permanently delete your account
+                                            and remove your data from our servers.
+                                        </DialogDescription>
+                                    </DialogHeader>
+                                </DialogContent>
+                            </Dialog>
+                        </DropdownMenuItem>
                     </DropdownMenuContent>
+
                 </DropdownMenu>
             )
         },
