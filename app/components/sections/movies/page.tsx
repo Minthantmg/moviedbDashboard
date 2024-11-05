df1050dfec631899f43bef04332594c6f8ea3dec
MinThant <minthant180@gmail.com>
2024-11-05 17:45:39 +0900

    feat:implement loading

--- app/components/sections/movies/page.tsx
+++ app/components/sections/movies/page.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import {useMovies} from "@/hooks/useMovies";
 import {DataTable} from "@/app/components/sections/movies/data-table";
 import {columns} from "@/app/components/sections/movies/columns";
+import Loading from "@/app/components/sections/loading";
 
 
-const AddUser = () => {
+const page = () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const {getMovieListHook} = useMovies()
     const {data: movie, isSuccess,isLoading ,isError} = getMovieListHook()
     return (
         <div>
             {isError && <>Error...</>}
-            {isLoading && <>Loading...</>}
+            {isLoading && <><Loading /></>}
             {isSuccess && (
                 <>
                     <div className='text-3xl mt-4'>Movie List</div>
@@ -24,4 +26,4 @@ const AddUser = () => {
     );
 };
 
-export default AddUser;
+export default page;
