c35841530487091aaa11e6daf01ca6aa52ff42fc
MinThant <minthant180@gmail.com>
2024-11-06 17:57:56 +0900

    fix:add state for handle

--- app/components/sections/users/page.tsx
+++ app/components/sections/users/page.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+'use client'
+import React, {useState} from 'react';
 import {useUser} from "@/hooks/useUser";
 import {DataTable} from "@/app/components/sections/users/data-table";
 import {columns} from "@/app/components/sections/users/columns";
 import Loading from "@/app/components/sections/loading";
+import AddUserSheet from "@/app/components/sections/users/components/AddUserSheet";
 
 const Page = () => {
     const {getUserListHook} = useUser()
     const {data: user,isLoading,isSuccess} = getUserListHook()
+    const [isAddDialogOpen, setAddDialogOpen] = useState(false);
+
     return (
         <div>
             {isLoading && <><Loading /></>}
@@ -14,6 +18,7 @@ const Page = () => {
                 <div>
                     <div className='text-3xl mt-4'>User and Admin List</div>
                     <div className='mt-4'>Total: {user.length} person</div>
+                    <AddUserSheet isOpen={isAddDialogOpen} setIsOpen={setAddDialogOpen} />
                     <DataTable columns={columns} data={user}/>
                 </div>
             )}
