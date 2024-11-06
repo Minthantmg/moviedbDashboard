d6df17530de00029f74e75d035116576c51ce84e
MinThant <minthant180@gmail.com>
2024-11-06 17:58:27 +0900

    feat:create user detail component

---
+++ app/components/sections/users/components/UserDetailSheet.tsx
@@ -0,0 +1,51 @@
+import {Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger} from "@/components/ui/sheet";
+import {User} from "@/type";
+
+interface UserDetailSheetProps {
+    user: User;
+}
+
+const UserDetailSheet: React.FC<UserDetailSheetProps> = ({user}) => {
+    return (
+        <Sheet>
+            <SheetTrigger asChild>
+                <div className="text-sm cursor-pointer py-2 hover:bg-gray-100 px-2">View Detail</div>
+            </SheetTrigger>
+            <SheetContent>
+                <SheetHeader>
+                    <SheetTitle>{user.name}</SheetTitle>
+                    <SheetDescription>
+                        <div className="text-black">
+                            <div className="flex mt-4">
+                                <div className="w-1/3">
+                                    Name
+                                </div>
+                                <div className="w-2/3 text-gray-500 break-all">
+                                    {user.email}
+                                </div>
+                            </div>
+                            <div className="flex mt-4">
+                                <div className="w-1/3">
+                                    Password
+                                </div>
+                                <div className="w-2/3 text-gray-500 break-all">
+                                    {user.password}
+                                </div>
+                            </div>
+                            <div className="flex mt-4">
+                                <div className="w-1/3">
+                                    Role
+                                </div>
+                                <div className="w-2/3 text-gray-500 break-all">
+                                    {user.role}
+                                </div>
+                            </div>
+                        </div>
+                    </SheetDescription>
+                </SheetHeader>
+            </SheetContent>
+        </Sheet>
+    );
+};
+
+export default UserDetailSheet;
