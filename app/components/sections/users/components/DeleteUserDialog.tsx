0a232ac54f52f2c0cb7ac9850dae034ca2e3b1fe
MinThant <minthant180@gmail.com>
2024-11-06 17:57:01 +0900

    feat:update delete User component

---
+++ app/components/sections/users/components/DeleteUserDialog.tsx
@@ -0,0 +1,35 @@
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger, DialogFooter } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import {User} from "@/type";
+import {Dispatch, SetStateAction} from "react";
+
+
+interface DeleteUserDialogProps {
+    isOpen: boolean;
+    setIsOpen: Dispatch<SetStateAction<boolean>>;
+    user: User;
+}
+
+const DeleteUserDialog: React.FC<DeleteUserDialogProps> = ({ isOpen, setIsOpen, user }) => {
+    return (
+        <Dialog open={isOpen} onOpenChange={setIsOpen}>
+            <DialogTrigger asChild>
+                <div className="text-sm cursor-pointer py-2 hover:bg-gray-100 px-2">Delete</div>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Are you absolutely sure?</DialogTitle>
+                    <DialogDescription>
+                        This action cannot be undone. This will permanently delete your account and remove your data from our servers.
+                    </DialogDescription>
+                </DialogHeader>
+                <DialogFooter>
+                    <Button variant="outline" onClick={() => setIsOpen(false)}>Cancel</Button>
+                    <Button variant="destructive" onClick={() => { /* Perform delete action */ setIsOpen(false); }}>Delete</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    );
+};
+
+export default DeleteUserDialog;
