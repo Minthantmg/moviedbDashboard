6d0e9442dba613c661d2866c43edb7c7bf7e174b
MinThantMaung <minthant180@gmail.com>
2024-11-07 12:01:41 +0900

    fix:implement addmovie component

--- app/components/sections/movies/page.tsx
+++ app/components/sections/movies/page.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+"use client"
+import React, {useState} from 'react';
 import {useMovies} from "@/hooks/useMovies";
 import {DataTable} from "@/app/components/sections/movies/data-table";
 import {columns} from "@/app/components/sections/movies/columns";
 import Loading from "@/app/components/sections/loading";
-import {Button} from "@/components/ui/button";
+import AddMovieSheet from "@/app/components/sections/movies/components/AddMovieSheet";
 
 
 const page = () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [isAddMovieDialogOpen, setAddMovieDialogOpen] = useState(false);
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const {getMovieListHook} = useMovies()
     const {data: movie, isSuccess,isLoading ,isError} = getMovieListHook()
@@ -18,7 +21,7 @@ const page = () => {
                 <>
                     <div className='text-3xl mt-4'>Movie List</div>
                     <div className='mt-4'>Total: {movie.length} movies</div>
-                    <Button variant="outline" className="mt-4">Add Movie</Button>
+                    <AddMovieSheet isOpen={isAddMovieDialogOpen} setIsOpen={setAddMovieDialogOpen} />
                     <div>
                         <DataTable columns={columns} data={movie}/>
                     </div>
