c583d8dd426ad6c0da8bf31ec474df291d5f7ddd
MinThantMaung <minthant180@gmail.com>
2024-11-07 17:39:18 +0900

    feat:create createUser api call

--- api/user.ts
+++ api/user.ts
@@ -1,5 +1,12 @@
 import {axiosInstance} from "@/utills/axiosInstance";
 
+interface CreateUserParams {
+    name: string;
+    email: string;
+    password: string;
+    role: 'admin' | 'user'
+}
+
 export const getUserList = async () => {
     try {
         const res = await axiosInstance.get("/users");
@@ -8,3 +15,12 @@ export const getUserList = async () => {
         throw e;
     }
 };
+
+export const createUser = async ({name, email, password,role}:CreateUserParams) => {
+    try {
+        const res = await axiosInstance.post("/register",{name, email, password,role});
+        return res.data;
+    }catch (e){
+        throw e;
+    }
+}
