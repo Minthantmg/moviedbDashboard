e9a619e22dc824118914617a59c62b87536846ad
MinThantMaung <minthant180@gmail.com>
2024-11-07 17:39:33 +0900

    fix:delete unused import

--- hooks/useMovies.ts
+++ hooks/useMovies.ts
@@ -1,5 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import {getMovieList} from "@/api/movie";
+import {Movie} from "@/type";
+import {createUser} from "@/api/user";
 
 const getMovieListHook = () => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
