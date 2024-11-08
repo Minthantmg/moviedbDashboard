584189e772149dd40acda8949ab4fc8b49a3f9a2
MinThantMaung <minthant180@gmail.com>
2024-11-08 17:20:02 +0900

    feat:add delete api call

--- app/components/sections/users/components/DeleteUserDialog.tsx
+++ app/components/sections/users/components/DeleteUserDialog.tsx
@@ -1,8 +1,9 @@
+'use client'
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger, DialogFooter } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import {User} from "@/type";
 import {Dispatch, SetStateAction} from "react";
-
+import {useUser} from "@/hooks/useUser";
 
 interface DeleteUserDialogProps {
     isOpen: boolean;
@@ -10,7 +11,24 @@ interface DeleteUserDialogProps {
     user: User;
 }
 
-const DeleteUserDialog: React.FC<DeleteUserDialogProps> = ({ isOpen, setIsOpen }) => {
+const DeleteUserDialog: React.FC<DeleteUserDialogProps> = ({ isOpen, setIsOpen ,user}) => {
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const {deleteUserHook} = useUser()
+    const { mutateAsync: deleteData, isPending } = deleteUserHook();
+
+    console.log(user);
+    const handleDelete = async () => {
+        try {
+            console.log('Deleting user with ID:', user._id);
+            await deleteData(user._id);
+            setIsOpen(false);
+            console.log('User deleted successfully');
+        } catch (error) {
+            console.error('Error deleting user:', error);
+        }
+    };
+
     return (
         <Dialog open={isOpen} onOpenChange={setIsOpen}>
             <DialogTrigger asChild>
@@ -25,7 +43,9 @@ const DeleteUserDialog: React.FC<DeleteUserDialogProps> = ({ isOpen, setIsOpen }
                 </DialogHeader>
                 <DialogFooter>
                     <Button variant="outline" onClick={() => setIsOpen(false)}>Cancel</Button>
-                    <Button variant="destructive" onClick={() => { /* Perform delete action */ setIsOpen(false); }}>Delete</Button>
+                    <Button variant="destructive" onClick={handleDelete}>
+                        {isPending ? 'Deleting...' : 'Delete'}
+                    </Button>
                 </DialogFooter>
             </DialogContent>
         </Dialog>
