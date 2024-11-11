08d541ca507c6c01df677b29228dfba9f37a6390
MinThantMaung <minthant180@gmail.com>
2024-11-11 11:51:23 +0900

    feat:install necessary package

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,6 +59,6 @@ const config: Config = {
   	}
   },
 	// eslint-disable-next-line @typescript-eslint/no-require-imports
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('daisyui')],
 };
 export default config;
