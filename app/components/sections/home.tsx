5c683a643dff526f6b20c5e70683c42ed943dd78
MinThantMaung <minthant180@gmail.com>
2024-11-11 15:35:58 +0900

    feat:add Dougnnut chart

--- app/components/sections/home.tsx
+++ app/components/sections/home.tsx
@@ -1,11 +1,91 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
+import { useUser } from '@/hooks/useUser';
+import { useMovies } from '@/hooks/useMovies';
+import Loading from '@/app/components/sections/loading';
+import { Doughnut } from 'react-chartjs-2';
+import { Chart, Tooltip, Legend, ArcElement } from 'chart.js';
+import {Movie} from "@/type";
+
+Chart.register(Tooltip, Legend, ArcElement);
 
 const Home = () => {
+    const { getUserListHook } = useUser();
+    const { data: users, isSuccess: isUserSuccess, isLoading: isUserLoading } = getUserListHook();
+
+    const { getMovieListHook } = useMovies();
+    const { data: movies, isSuccess, isLoading } = getMovieListHook();
+
+    const [movieChartData, setMovieChartData] = useState({
+        labels: [],
+        datasets: [
+            {
+                data: [],
+                backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF'],
+            },
+        ],
+    });
+
+    const options = {
+        plugins: {
+            legend: {
+                display: true,
+                position: 'right',
+            },
+        },
+    };
+
+
+    useEffect(() => {
+        if (movies && movies.length > 0) {
+            const genreCounts = movies.reduce((acc: { [key: string]: number }, movie: Movie) => {
+                movie.genre.forEach((genre) => {
+                    acc[genre] = (acc[genre] || 0) + 1;
+                });
+                return acc;
+            }, {});
+
+            const labels = Object.keys(genreCounts);
+            const data = labels.map((genre) => genreCounts[genre]);
+
+            setMovieChartData({
+                labels: labels.map((genre) => `${genre}`),
+                datasets: [
+                    {
+                        data: data,
+                        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF'],
+                    },
+                ],
+            });
+        }
+    }, [movies]);
+
     return (
         <div>
-            <div className='text-3xl mt-4'>
-            Dashboard
-            </div>
+            {(isUserLoading || isLoading) && <Loading />}
+            {isSuccess && isUserSuccess && (
+                <>
+                    <div className='text-3xl mt-4'>
+                        Dashboard
+                    </div>
+                    <div className="stats shadow mt-10">
+                        <div className="stat">
+                            <div className="stat-title">Total Movies</div>
+                            <div className="stat-value text-primary">{movies ? movies.length : 0}</div>
+                        </div>
+
+                        <div className="stat">
+                            <div className="stat-title">Total Users</div>
+                            <div className="stat-value text-primary">{users ? users.length : 0}</div>
+                        </div>
+                    </div>
+                    <div className="flex justify-center items-center mt-16">
+                        <div className="w-full h-96 mr-10">
+                            <Doughnut options={options} data={movieChartData}/>
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
     );
 };
