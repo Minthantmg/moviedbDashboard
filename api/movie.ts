ceeabaa7d43ce81d37cd92dc62512e35b7e14202
MinThantMaung <minthant180@gmail.com>
2024-11-11 17:41:28 +0900

    feat:added create movie route

--- api/movie.ts
+++ api/movie.ts
@@ -1,11 +1,34 @@
 import { axiosInstance } from "@/utills/axiosInstance";
+import {Comment} from "@/type";
+
+export interface CreateMovieParams {
+    title: string;
+    description: string;
+    releaseDate: string;
+    genre: string[];
+    rating: number;
+    director: string;
+    cast: string[];
+    duration: number;
+    comments: Comment[];
+    createdAt: string;
+}
 
 export const getMovieList = async () => {
     try {
         const res = await axiosInstance.get("/movies");
+        console.log(res.data);
         return res.data;
     } catch (e) {
         throw e;
     }
 };
 
+export const createMovie = async (movieData: CreateMovieParams) => {
+    try {
+        const res = await axiosInstance.post("/movies", movieData);
+        return res.data;
+    } catch (e) {
+        throw e;
+    }
+}
