8f857d8aa9a9d81d7e9233ef8a0b6d1638bba8e4
MinThantMaung <minthant180@gmail.com>
2024-11-12 15:00:01 +0900

    fix:api call route update

--- hooks/useMovies.ts
+++ hooks/useMovies.ts
@@ -1,6 +1,7 @@
-import {useMutation, useQuery} from "@tanstack/react-query";
-import {createMovie, deleteMovie, getMovieList} from "@/api/movie";
+import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
+import {createMovie, deleteMovie, getMovieList, updateMovie} from "@/api/movie";
 import { CreateMovieParams } from "@/api/movie";
+import {Movie} from "@/type";
 
 const getMovieListHook = () => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -24,10 +25,30 @@ const deleteMovieHook = () => {
     })
 }
 
+const updateMovieHook = () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const queryClient = useQueryClient();
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    return useMutation({
+        mutationKey: ["update", "movie"],
+        mutationFn: ({ movieId, updatedData }: { movieId: string; updatedData: Partial<Movie> }) =>
+            updateMovie(movieId, updatedData),
+        onSuccess: () => {
+            try {
+                // @ts-ignore
+                queryClient.invalidateQueries(["get", "movies"]);
+            } catch (error) {
+                console.error('Error while invalidating query:', error);
+            }
+        },
+    })
+}
+
 export const useMovies = () => {
     return {
         getMovieListHook,
         createMovieHook,
-        deleteMovieHook
+        deleteMovieHook,
+        updateMovieHook
     };
 };
