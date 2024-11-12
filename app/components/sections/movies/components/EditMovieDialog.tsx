54d6519350b8c6da19bd5eb6ca520cec53e91214
MinThantMaung <minthant180@gmail.com>
2024-11-12 17:58:30 +0900

    fix:update ui

--- app/components/sections/movies/components/EditMovieDialog.tsx
+++ app/components/sections/movies/components/EditMovieDialog.tsx
@@ -20,6 +20,7 @@ const EditMovieDialog:React.FC<EditMovieDialogProps> = ({isOpen,setIsOpen,movie}
     const {updateMovieHook} = useMovies()
     const { mutateAsync: updateData, isPending} = updateMovieHook()
 
+    console.log("hello")
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
         const { name, value } = e.target;
         setFormData((prevData:Movie) => ({
